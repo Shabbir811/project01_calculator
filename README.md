c17910b36910f2668685c68c4ffb416f6347d656
shabbir811 <uandi.ss8.ss@gmail.com>
2024-04-09 22:51:52 +0500

    write some clean code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# project01_calculator
+# project01_calculator 
+it is A cli based simple calculator with 
+addition,
+subtraction,
+multiplication,
+dividsion,
+power base,
+remainder.
+percentage.
+
--- simple_calculator.js
+++ simple_calculator.js
@@ -2,6 +2,7 @@
 //first we import inquirer.
 import inquirer from "inquirer";
 import chalk from "chalk";
+let list = ["+", "-", "x", "/", "power X^n", "remainder", "%"];
 // we let input from user through inquirer 
 const answer = await inquirer.prompt([
     {
@@ -13,7 +14,7 @@ const answer = await inquirer.prompt([
         name: "opration",
         type: "list",
         message: "select your oprations",
-        choices: ["+", "-", "x", "/", "%", "power X^n", "remainder"]
+        choices: list
     },
     {
         name: "num2",
@@ -25,30 +26,39 @@ const answer = await inquirer.prompt([
 let result = (answer.opration);
 let num1 = answer.num1;
 let num2 = answer.num2;
+//perform some calcultions
+let sum = num1 + num2;
+let minus = num1 - num2;
+let multiple = num1 * num2;
+let divide = num1 / num2;
+let power1 = num1 ** num2;
+let remainder1 = num1 % num2;
+let percentage1 = (num2 / num1) * 100;
+let percentage2 = (num1 / num2) * 100;
 //use some if else conditions to create logic for code
-if (result === "+") {
-    console.log(chalk.blue.bgBlack `your answer is${num1 + num2}`);
+if (result.includes(list[0])) {
+    console.log(chalk.blue.bgBlack `your answer is${sum}`);
 }
-else if (result === "-") {
-    console.log(chalk.red.bgBlack `your answer is${num1 - num2}`);
+else if (result.includes(list[1])) {
+    console.log(chalk.red.bgBlack `your answer is${minus}`);
 }
-else if (result === "x") {
-    console.log(chalk.yellow.bgBlack `your answer is${num1 * num2}`);
+else if (result.includes(list[2])) {
+    console.log(chalk.yellow.bgBlack `your answer is${multiple}`);
 }
-else if (result === "/") {
-    console.log(chalk.green.bgBlack `your answer is${num1 / num2}`);
+else if (result.includes(list[3])) {
+    console.log(chalk.green.bgBlack `your answer is${divide}`);
 }
-else if (result === "power X^n") {
-    console.log(chalk.cyan.bgBlack `your answer is${num1 ** num2}`);
+else if (result.includes(list[5])) {
+    console.log(chalk.cyan.bgBlack `your answer is${power1}`);
 }
-else if (result === "remainder") {
-    console.log(chalk.rgb(80, 130, 220).bgBlack `your remainder is${num1 % num2}`);
+else if (result.includes(list[4])) {
+    console.log(chalk.rgb(80, 130, 220).bgBlack `your remainder is${remainder1}`);
 }
-else if (result === "%") {
+else if (result.includes(list[6])) {
     if (num1 > num2) {
-        console.log(chalk.redBright.bgBlack `your percentage is${(num2 / num1) * 100}`);
+        console.log(chalk.redBright.bgBlack `your percentage is${percentage1}`);
     }
     else {
-        console.log(chalk.redBright.bgBlack `your percentage is${(num1 / num2) * 100}`);
+        console.log(chalk.redBright.bgBlack `your percentage is${percentage2}`);
     }
 }
